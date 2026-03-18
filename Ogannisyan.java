c91bbcfb0ba1c25751b1472bd6595dbaa560d2af
neformal2005 <robert.ogannisian2015@yandex.ru>
2026-03-18 20:32:09 +0300

    first RobertBranch commit

--- Ogannisyan.java
+++ Ogannisyan.java
@@ -7,7 +7,7 @@ public abstract class MyTimer {
 
     private List<TimerListener> listeners = new ArrayList<>();
     protected Timer executor = new Timer();
-
+    // коммит в свою ветку
     public void addListener(TimerListener listener) {
         listeners.add(listener);
     }
